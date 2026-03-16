92a6e731c9339e9cb1a6a82ab90454e2ee97b6fd
conterb80 <conterb80@gmail.com>
2026-03-16 14:41:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 window.TURNI_APP_CONFIG = {
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
+    apiKey: "AIzaSyDsDSbb0j6VrkQMhZRAVAredHI8BQdI57Q",
+    authDomain: "turni-di-famiglia.firebaseapp.com",
+    databaseURL: "https://turni-di-famiglia-default-rtdb.firebaseio.com",
+    projectId: "turni-di-famiglia",
+    storageBucket: "turni-di-famiglia.firebasestorage.app",
+    messagingSenderId: "940441790090",
+    appId: "1:940441790090:web:9ab729eafe46ef06fdaf73"
   },
   sharedPath: "turni-famiglia"
 };
