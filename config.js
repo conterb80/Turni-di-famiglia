6e0a92b86091f84708eb69ccb50bcf6dbb9f2b0b
conterb80 <conterb80@gmail.com>
2026-03-16 14:25:07 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+window.TURNI_APP_CONFIG = {
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  },
+  sharedPath: "turni-famiglia"
+};
