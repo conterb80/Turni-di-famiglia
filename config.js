9e14a828b13e4b4c7804742bf4681cc515018e82
conterb80 <conterb80@gmail.com>
2026-05-23 08:14:26 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.TURNI_APP_CONFIG = {
   firebase: {
     apiKey: "AIzaSyDsDSbb0j6VrkQMhZRAVAredHI8BQdI57Q",
-    authDomain: "turni-di-famiglia.firebaseapp.com",
+    authDomain: "turni-di-famiglia-99d41.firebaseapp.com",
     databaseURL: "https://turni-di-famiglia-99d41-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "turni-di-famiglia",
-    storageBucket: "turni-di-famiglia.firebasestorage.app",
+    projectId: "turni-di-famiglia-99d41",
+    storageBucket: "turni-di-famiglia-99d41.firebasestorage.app",
     messagingSenderId: "940441790090",
     appId: "1:940441790090:web:9ab729eafe46ef06fdaf73"
   },
